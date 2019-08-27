dd25301cac4f2ad3ef4c7687fe141a10fa45326d
Igor <igorocampos@gmail.com>
2019-08-27 08:34:44 -0300

    Improvements in Add method

--- Commands.cs
+++ Commands.cs
@@ -165,7 +165,7 @@ namespace ESCPOS
             var height = new byte[] { 0x1D, 0x68, (byte)heightInDots };
             var settings = new byte[] { 0x1D, 0x6B, (byte)barCodeType, (byte)barCode.Length };
             var bar = Encoding.UTF8.GetBytes(barCode);
-            return height.Add(settings).Add(bar);
+            return height.Add(settings, bar);
         }
 
         /// <summary>
@@ -182,7 +182,7 @@ namespace ESCPOS
             var storeData = new byte[] { 0x1D, 0x28, 0x6B, (byte)pL, (byte)pH, 0x31, 0x50, 0x30 };
             var data = Encoding.UTF8.GetBytes(content);
             var print = new byte[] { 0x1D, 0x28, 0x6B, 0x03, 0x00, 0x31, 0x51, 0x30 };
-            return model.Add(size).Add(errorCorrection).Add(storeData).Add(data).Add(print);
+            return model.Add(size, errorCorrection, storeData, data, print);
         }
 
     }
--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -8,10 +8,23 @@ namespace ESCPOS.Utils
     {
         public static byte[] Add(this byte[] array1, byte[] array2)
         {
+            if (array1 is null)
+                return array2;
+
+            if (array2 is null)
+                return array1;
+
             byte[] result = new byte[array1.Length + array2.Length];
             Array.Copy(array1, result, array1.Length);
             Array.Copy(array2, 0, result, array1.Length, array2.Length);
             return result;
+        }
+        public static byte[] Add(this byte[] array1, params byte[][] arrays)
+        {
+            foreach (byte[] array2 in arrays)
+                array1 = array1.Add(array2);
+
+            return array1;
         }
     }
 
