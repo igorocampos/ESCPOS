2db4deae5f08fc14f9570685c74f732e4d216fc4
Igor <igorocampos@gmail.com>
2019-08-27 08:12:47 -0300

    namespace change

--- Commands.cs
+++ Commands.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESCPOS.Utils;
+using System;
 using System.Text;
 
 namespace ESCPOS
--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ESCPOS
+namespace ESCPOS.Utils
 {
     public static class Utils
     {
