30d5b13c9a81331918534a200901413db93671f0
Igor <igorocampos@gmail.com>
2019-08-27 10:37:25 -0300

    Adding some Unit Tests

---
+++ UnitTest/StringDiffHelper.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    //For more information, please see https://haacked.com/archive/2012/01/14/comparing-strings-in-unit-tests.aspx/
+    public static class StringDiffHelper
+    {
+        public static void ShouldEqualWithDiff(string actualValue, string expectedValue, DiffStyle diffStyle = DiffStyle.Full)
+            => ShouldEqualWithDiff(actualValue, expectedValue, diffStyle, Console.Out);
+
+        public static void ShouldEqualWithDiff(string actualValue, string expectedValue, DiffStyle diffStyle, TextWriter output)
+        {
+            if (actualValue is null || expectedValue is null)
+            {
+                Assert.AreEqual(expectedValue, actualValue);
+                return;
+            }
+
+            if (actualValue.Equals(expectedValue, StringComparison.Ordinal))
+                return;
+
+            output.WriteLine("  Idx Expected  Actual");
+            output.WriteLine("-------------------------");
+            int maxLen = Math.Max(actualValue.Length, expectedValue.Length);
+            int minLen = Math.Min(actualValue.Length, expectedValue.Length);
+            for (int i = 0; i < maxLen; i++)
+                if (diffStyle != DiffStyle.Minimal || i >= minLen || actualValue[i] != expectedValue[i])
+                    //                                  put a mark beside a differing row                 index                         character decimal value                                             character safe string                                                    character decimal value                                                character safe string
+                    output.WriteLine($"{(i < minLen && actualValue[i] == expectedValue[i] ? " " : " * ")} {i,-3} {(i < expectedValue.Length ? ((int)expectedValue[i]).ToString() : ""),-4} {(i < expectedValue.Length ? expectedValue[i].ToSafeString() : ""),-3}  {(i < actualValue.Length ? ((int)actualValue[i]).ToString() : ""),-4} {(i < actualValue.Length ? actualValue[i].ToSafeString() : ""),-3}");
+
+            output.WriteLine();
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        private static string ToSafeString(this char c)
+        {
+            if (char.IsControl(c) || char.IsWhiteSpace(c))
+                switch (c)
+                {
+                    case '\r': return @"\r";
+                    case '\n': return @"\n";
+                    case '\t': return @"\t";
+                    case '\a': return @"\a";
+                    case '\v': return @"\v";
+                    case '\f': return @"\f";
+                    default: return $"\\u{(int)c:X};";
+                }
+
+            return c.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    public enum DiffStyle
+    {
+        Full,
+        Minimal
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,93 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ESCPOS.Commands;
+using static UnitTest.StringDiffHelper;
+using ESCPOS.Utils;
+using System.IO;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        const string TEST_FILE = "test.txt";
+        const string TEXT_DATA = "Data test for QRCode with some special characters: $ñáãç*/&#@\"'^{}";
+
+        [TestMethod]
+        public void QRCode_NoParameter()
+        {
+            PrintQRCode(TEXT_DATA).Print(TEST_FILE);
+            ShouldEqualWithDiff("(k\01A1\0(k\01C(k\01E0(kE\01P0Data test for QRCode with some special characters: $ñáãç*/&#@\"'^{}(k\01Q0", File.ReadAllText(TEST_FILE));
+        }
+
+        [TestMethod]
+        public void QRCode_FullParameters()
+        {
+            PrintQRCode(TEXT_DATA, QRCodeModel.Model2, QRCodeCorrection.Percent30, QRCodeSize.Large).Print(TEST_FILE);
+            ShouldEqualWithDiff("(k\01A2\0(k\01C(k\01E3(kE\01P0Data test for QRCode with some special characters: $ñáãç*/&#@\"'^{}(k\01Q0", File.ReadAllText(TEST_FILE));
+        }
+
+        [TestMethod]
+        public void Barcode_EAN8()
+        {
+            PrintBarCode(BarCodeType.EAN8, "90311017", 52).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "h4kD90311017");
+        }
+
+        [TestMethod]
+        public void Barcode_EAN13()
+        {
+            PrintBarCode(BarCodeType.EAN13, "9780201379624", 52).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "h4kC\r9780201379624");
+        }
+
+        [TestMethod]
+        public void Barcode_CODE128()
+        {
+            PrintBarCode(BarCodeType.CODE128, "ABC123456", 52).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "h4kI	ABC123456");
+        }
+
+        [TestMethod]
+        public void Barcode_UPC_A()
+        {
+            PrintBarCode(BarCodeType.UPC_A, "72527273070", 52).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "h4kA72527273070");
+        }
+
+        [TestMethod]
+        public void DoubleHeight()
+        {
+            SelectCharSizeHeight(CharSizeHeight.Double).Add(TEXT_DATA.ToBytes()).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "!Data test for QRCode with some special characters: $ñáãç*/&#@\"'^{}");
+        }
+
+        [TestMethod]
+        public void DoubleWidth()
+        {
+            SelectCharSizeWidth(CharSizeWidth.Double).Add(TEXT_DATA.ToBytes()).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "!Data test for QRCode with some special characters: $ñáãç*/&#@\"'^{}");
+        }
+
+        [TestMethod]
+        public void AlignCenter()
+        {
+            SelectJustification(Justification.Center).Add(TEXT_DATA.ToBytes()).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "aData test for QRCode with some special characters: $ñáãç*/&#@\"'^{}");
+        }
+
+        [TestMethod]
+        public void AlignRight()
+        {
+            SelectJustification(Justification.Right).Add(TEXT_DATA.ToBytes()).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "aData test for QRCode with some special characters: $ñáãç*/&#@\"'^{}");
+        }
+
+        [TestMethod]
+        public void AlignLeft()
+        {
+            SelectJustification(Justification.Left).Add(TEXT_DATA.ToBytes()).Print(TEST_FILE);
+            ShouldEqualWithDiff(File.ReadAllText(TEST_FILE), "a\0Data test for QRCode with some special characters: $ñáãç*/&#@\"'^{}");
+        }
+
+    }
+}
