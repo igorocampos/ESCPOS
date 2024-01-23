0b9f0ad0f62bf6447b0e803aa71e7a088ecb4b22
Igor <igorocampos@gmail.com>
2024-01-23 13:31:32 -0500

    Adding more simple commands

--- ESCPOS/ESCPOSCommands/Commands.cs
+++ ESCPOS/ESCPOSCommands/Commands.cs
@@ -6,85 +6,8 @@ using System.Text;
 
 namespace ESCPOS
 {
-    public static class Commands
+    public static partial class Commands
     {
-        #region Simple Commands
-        /// <summary>
-        /// Moves the print position to the next tab position.
-        /// </summary>
-        /// <remarks>
-        /// ·This command is ignored unless the next tab position has been set.
-        /// ·If the next horizontal tab position exceeds the printing area, the printer sets the printing position to[Printing area width + 1].
-        /// ·If this command is received when the printing position is at [printing area width +1], the printer executes print buffer-full printing of the current line and horizontal tab processing from the beginning of the next line.
-        /// ·The default setting of the horizontal tab position for the paper roll is font A (12 x 24) every 8th character(9th, 17th, 25th, … column).
-        /// </remarks>
-        public static byte[] HorizontalTab => new byte[] { 0x09 };
-
-        /// <summary>
-        /// Prints the data in the print buffer and feeds one line based on the current line spacing.
-        /// </summary>
-        /// <remarks>
-        /// ·This command sets the print position to the beginning of the line.
-        /// </remarks>
-        public static byte[] LineFeed => new byte[] { 0x0A };
-
-        /// <summary>
-        /// When automatic line feed is enabled, this command functions the same as LF, when automatic line feed is disabled, this command is ignored
-        /// </summary>
-        /// <remarks>
-        /// ·Sets the print starting position to the beginning of the line.
-        /// ·The automatic line feed is ignored.
-        /// </remarks>
-        public static byte[] CarriageReturn => new byte[] { 0x0D };
-
-        /// <summary>
-        /// Prints the data in the print buffer and returns to standard mode.
-        /// </summary>
-        /// <remarks>
-        /// ·The buffer data is deleted after being printed.
-        /// ·The printer does not execute paper cutting.
-        /// ·This command sets the print position to the beginning of the line.
-        /// ·This command is enabled only in page mode.
-        /// </remarks>
-        public static byte[] PrintAndReturnToStandardMode => new byte[] { 0x0C };
-
-        /// <summary>
-        /// In page mode, delete all the print data in the current printable area.
-        /// </summary>
-        /// <remarks>
-        /// ·This command is enabled only in page mode.
-        /// ·If data that existed in the previously specified printable area also exists in the currently specified printable area, it is deleted.
-        /// </remarks>
-        public static byte[] CancelPrint => new byte[] { 0x18 };
-
-        //Alias
-        public static byte[] HT => HorizontalTab;
-        public static byte[] LF => LineFeed;
-        public static byte[] CR => CarriageReturn;
-        public static byte[] FF => PrintAndReturnToStandardMode;
-        public static byte[] CAN => CancelPrint;
-        #endregion Simple Commands
-
-        /// <summary>
-        /// ESC @
-        /// </summary>
-        public static byte[] InitializePrinter => new byte[] { 0x1B, 0x40 };
-
-        /// <summary>
-        /// ESC p m t1 t2
-        /// </summary>
-        public static byte[] OpenDrawer => new byte[] { 0x1B, 0x70, 0x00, 0x3C, 0x78 };
-
-        /// <summary>
-        /// ESC m
-        /// </summary>
-        public static byte[] PaperCut => new byte[] { 0x1B, 0x6D };
-
-        /// <summary>
-        /// ESC i
-        /// </summary>
-        public static byte[] FullPaperCut => new byte[] { 0x1B, 0x69 };
-
         /// <summary>
         /// ESC ! n
         /// </summary>
